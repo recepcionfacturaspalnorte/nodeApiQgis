a5a585706abebef517dd35b471296906e6f393cf
Eduardo Ruiz <eduardoruiz19@hotmail.com>
2026-02-20 08:53:10 -0500

    Quitando lo innecesario

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,27 +1,6 @@
 import { Router } from 'express';
 
 
-import { PermissionRoutes } from './permission/routes';
-import { ProductRoutes } from './product/routes';
-
-
-import { UbicacionRoutes } from './ubicacion/routes';
-import { SucursalRoutes } from './sucursal/routes';
-import { Tipo_equipoRoutes } from './tipo_equipo/routes';
-import { Tipo_activoRoutes } from './tipo_activo/routes';
-import { FileUploadRoutes } from './file-upload/routes';
-import { ImageRoutes } from './images/routes';
-
-import { Tipo_asignacionRoutes } from './tipo_asignacion/routes';
-import { Activo_asignacionRoutes } from './activo_asignacion/routes';
-
-import { TicketRoutes } from './ticket/routes';
-import { TicketImageRoutes } from './ticketImage/routes';
-import { PrioridadRoutes } from './prioridad/routes';
-import { Plan_mantenimientoRoutes } from './plan-mantenimiento/routes';
-
-
-
 import { QgisRoutes } from './qgis/routes';
 
 
