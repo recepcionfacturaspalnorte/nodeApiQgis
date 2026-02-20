fd638d40eddc47ab8b86bf859096ac873b881ea4
Eduardo Ruiz <eduardoruiz19@hotmail.com>
2026-02-20 08:59:42 -0500

    ajuste

--- src/services/qgis.service.ts
+++ src/services/qgis.service.ts
@@ -1,12 +1,6 @@
 import { prisma } from "../data/postgres";
-import { ActivoDto } from "../domain/dtos/activo/activo.dto";
 
 
-import { PermissionDto } from "../domain/dtos/permission/permission.dto";
-import { PaginationDto } from "../domain/dtos/shared/pagination.dto";
-import { SucursalDto } from "../domain/dtos/sucursal/sucursal.dto";
-import { UbicacionDto } from "../domain/dtos/ubicacion/ubicacion.dto";
-import { CustomError } from "../domain/errors/custom.error";
 
 
 export class QgisService {
